25589e81db3b6cba955bc7abd8e30a6af49f6fb9
Marta Molina <marta.molina@live.u-tad.com>
2021-10-10 13:37:03 +0200

    Ya suma

--- js/app.js
+++ js/app.js
@@ -1,23 +1,42 @@
 /*
  * @author: Marta Molina Aguilera
  */
-const simbolo = "";
-
 $(document).ready(() => {
-  $("#btnSumar").click(() => {
+  let simbolo = "";
+  let num1 = 1.0;
+  let num2 = 1.0;
+  let resultado = 1.0;
+
+  const enableSegundoCampo = () => {
     $("#segundo-num").removeClass("opacity-dis");
     $("#formGroupExampleInput2").removeAttr("disabled");
+  };
+  const recogernum1 = () => {
+    num1 = parseInt($("#formGroupExampleInput").val());
+  };
+  const recogernum2 = () => {
+    num2 = parseInt($("#formGroupExampleInput2").val());
+  };
+
+  $("#btnSumar").click(() => {
+    enableSegundoCampo();
   });
   $("#btnRestar").click(() => {
-    $("#segundo-num").removeClass("opacity-dis");
-    $("#formGroupExampleInput2").removeAttr("disabled");
+    enableSegundoCampo();
   });
   $("#btnDividir").click(() => {
-    $("#segundo-num").removeClass("opacity-dis");
-    $("#formGroupExampleInput2").removeAttr("disabled");
+    enableSegundoCampo();
   });
+
   $("#btnMultiplicar").click(() => {
-    $("#segundo-num").removeClass("opacity-dis");
-    $("#formGroupExampleInput2").removeAttr("disabled");
+    enableSegundoCampo();
+  });
+
+  $("#btnIgual").click(() => {
+    recogernum1();
+    recogernum2();
+    resultado = num1 + num2;
+    console.log(resultado);
+    $("#muchotexto").text(`Resultado: ${resultado}`);
   });
 });
