d28b69f0fe0530fe9f137b3c2aa85ec46cde8e29
Marta Molina <marta.molina@live.u-tad.com>
2021-10-11 13:07:08 +0200

    Avances antes de ir a trabajar

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * @author: Marta Molina Aguilera
  */
 $(document).ready(() => {
-  let simbolo = "";
+  let operacion = "";
   let num1 = 1.0;
   let num2 = 1.0;
   let resultado = 1.0;
@@ -11,32 +11,87 @@ $(document).ready(() => {
     $("#segundo-num").removeClass("opacity-dis");
     $("#formGroupExampleInput2").removeAttr("disabled");
   };
-  const recogernum1 = () => {
-    num1 = parseInt($("#formGroupExampleInput").val());
+  const recogerNums = () => {
+    if (
+      operacion === "sumar" ||
+      operacion === "restar" ||
+      operacion === "multiplicar" ||
+      operacion === "dividir" ||
+      operacion === "seno" ||
+      operacion === "coseno" ||
+      operacion === "tangente" ||
+      operacion === "cotangente"
+    ) {
+      num1 = parseFloat($("#formGroupExampleInput").val());
+      num2 = parseFloat($("#formGroupExampleInput2").val());
+    } else {
+      num1 = parseFloat($("#formGroupExampleInput").val());
+    }
   };
-  const recogernum2 = () => {
-    num2 = parseInt($("#formGroupExampleInput2").val());
+  const operar = () => {
+    if (operacion === "sumar") {
+      resultado = num1 + num2;
+    } else if (operacion === "restar") {
+      resultado = num1 - num2;
+    } else if (operacion === "multiplicar") {
+      resultado = num1 * num2;
+    } else if (operacion === "dividir") {
+      resultado = num1 / num2;
+    } else if (operacion === "seno") {
+      //-------------------------------------------------Voy por aquí
+    } else if (operacion === "coseno") {
+    } else if (operacion === "tangente") {
+    } else if (operacion === "cotangente") {
+    } else if (operacion === "raiz") {
+    }
+  };
+  const annadirResultado = (resultado) => {
+    console.log(resultado);
+    $("#muchotexto").text(`Resultado: ${resultado}`);
   };
 
+  //Events listeners
   $("#btnSumar").click(() => {
+    operacion = "sumar";
     enableSegundoCampo();
   });
+
   $("#btnRestar").click(() => {
+    operacion = "restar";
+    enableSegundoCampo();
+  });
+  $("#btnMultiplicar").click(() => {
+    operacion = "multiplicar";
     enableSegundoCampo();
   });
   $("#btnDividir").click(() => {
+    operacion = "dividir";
     enableSegundoCampo();
   });
 
-  $("#btnMultiplicar").click(() => {
+  $("#btnRaiz").click(() => {
+    operacion = "raiz";
+  });
+
+  $("#btnSeno").click(() => {
+    operacion = "seno";
+    enableSegundoCampo();
+  });
+  $("#btnCoseno").click(() => {
+    operacion = "coseno";
+    enableSegundoCampo();
+  });
+  $("#btnTangente").click(() => {
+    operacion = "tangente";
+    enableSegundoCampo();
+  });
+  $("#btnCotangente").click(() => {
+    operacion = "cotangente";
     enableSegundoCampo();
   });
 
   $("#btnIgual").click(() => {
-    recogernum1();
-    recogernum2();
-    resultado = num1 + num2;
-    console.log(resultado);
-    $("#muchotexto").text(`Resultado: ${resultado}`);
+    recogerNums();
+    annadirResultado(resultado);
   });
 });
