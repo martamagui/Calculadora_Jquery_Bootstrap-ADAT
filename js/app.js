c6d08015b879dc2c11e462ce113de5f255d73a61
Marta Molina <marta.molina@live.u-tad.com>
2021-10-14 00:57:40 +0200

    funcionando

--- js/app.js
+++ js/app.js
@@ -31,31 +31,42 @@ $(document).ready(() => {
    */
   const recogerNums = () => {
     if (
-      operacion === "sumar" ||
-      operacion === "restar" ||
-      operacion === "multiplicar" ||
-      operacion === "dividir"
+      operacion === "+" ||
+      operacion === "-" ||
+      operacion === "x" ||
+      operacion === ":"
     ) {
       num1 = parseFloat($("#formGroupExampleInput").val());
       num2 = parseFloat($("#formGroupExampleInput2").val());
     } else {
-      num1 = parseFloat($("#formGroupExampleInput").val());
     }
+    num1 = parseFloat($("#formGroupExampleInput").val());
   };
 
   /*
-  Ejecuta la operación oportuna en función de la operación seleccionada.
+  Ejecuta la operación aritmética oportuna en función de la 
+  operación seleccionada, creando un String en la que se muestra esta 
+  y su resultado
   */
   const operar = () => {
     if (operacion === "+") {
-      resultado = num1 + num2;
+      resultado = `${num1} + ${num2} = ${num1 + num2}`;
     } else if (operacion === "-") {
-      resultado = num1 - num2;
+      resultado = `${num1} - ${num2} = ${num1 - num2}`;
     } else if (operacion === "x") {
-      resultado = num1 * num2;
+      resultado = `${num1} x ${num2} = ${num1 * num2}`;
     } else if (operacion === ":") {
-      resultado = num1 / num2;
+      resultado = `${num1} : ${num2} = ${num1 / num2}`;
+    } else if (operacion === "√") {
+      resultado = ` √${num1} : ${Math.sqrt(num1)}`;
+    } else if (operacion === "Seno") {
+      resultado = ` Seno de ${num1} : ${Math.sin(num1)}`;
+    } else if (operacion === "Coseno") {
+      resultado = ` Coseno de ${num1} : ${Math.cos(num1)}`;
+    } else if (operacion === "Tangente") {
+      resultado = ` Tangente de ${num1} : ${Math.tan(num1)}`;
     } else {
+      resultado = ` Cotangente de ${num1}: ${1 / Math.tan(num1)}`;
     }
   };
 
@@ -64,22 +75,14 @@ $(document).ready(() => {
    */
   const annadirResultado = (resultado) => {
     console.log(resultado);
-    $("#muchotexto").text(`Resultado: ${resultado}`);
+    $("#muchotexto").text(resultado);
   };
 
+  /*
+  Añade la operación al historial
+   */
   const appenAlHistorial = () => {
-    if (
-      operacion === "+" ||
-      operacion === "-" ||
-      operacion === "x" ||
-      operacion === ":"
-    ) {
-      $("#historial").prepend(
-        `${num1} ${operacion} ${num2} = ${resultado}<br>`
-      );
-    } else {
-      $("#historial").prepend(`${resultado}<br>`);
-    }
+    $("#historial").prepend(`${resultado}<br>`);
   };
 
   /*
@@ -130,35 +133,30 @@ $(document).ready(() => {
   /*Escucha cuando se pulsa "btnRaiz" del DOM y cambia la operación a "√", llama a mostrarResultado() */
   $("#btnRaiz").click(() => {
     operacion = "√";
-    resultado = ` Raíz: ${Math.sqrt(num1)}`;
     mostrarResultado();
   });
 
   /*Escucha cuando se pulsa "btnSeno" del DOM y cambia la operación a "Seno:" , llama a mostrarResultado() */
   $("#btnSeno").click(() => {
-    operacion = "Seno:";
-    resultado = ` Seno: ${Math.sin(num1)}`;
+    operacion = "Seno";
     mostrarResultado();
   });
 
   /*Escucha cuando se pulsa "btnCoseno" del DOM y cambia la operación a "Coseno:", llama a mostrarResultado() */
   $("#btnCoseno").click(() => {
-    operacion = "Coseno:";
-    resultado = ` Coseno: ${Math.cos(num1)}`;
+    operacion = "Coseno";
     mostrarResultado();
   });
 
   /*Escucha cuando se pulsa "btnTan" y cambia la operación a "Tangente:", llama a mostrarResultado() */
   $("#btnTan").click(() => {
-    operacion = "Tangente:";
-    resultado = ` Tangente: ${Math.tan(num1)}`;
+    operacion = "Tangente";
     mostrarResultado();
   });
 
   /*Escucha cuando se pulsa "btnCotangente" y cambia la operación a "Cotangente:", llama a mostrarResultado() */
   $("#btnCotangente").click(() => {
-    operacion = "Cotangente:";
-    resultado = ` Cotangente: ${1 / Math.tan(num1)}`;
+    operacion = "Cotangente";
     mostrarResultado();
   });
 
